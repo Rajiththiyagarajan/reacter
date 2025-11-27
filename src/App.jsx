824f1416949b1fb2c08606c04a5ce5b703dc6dae
Rajiththiyagarajan <161693298+Rajiththiyagarajan@users.noreply.github.com>
2025-11-27 11:38:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+function App() {
+
+  const [name,setname]=useState("");
+
+
+  const handler = (e) =>{
+    e.preventDefault();
+    alert(name);
+  }
+
+
+
+  return (
+    <form onSubmit={handler}>
+
+      <input type='text ' placeholder='enter name here ' value={name} onChange={(e) => setname(e.target.value)} />
+      <button type='submit'>submit</button>
+
+    </form>
+  )
+}
+
+export default App
